08acfc2ec25ae2e470d02e5967d532ea47ce192f
asa687 <85872834+asa687@users.noreply.github.com>
2025-08-09 21:49:53 +0100

    finalised a web scraper for both websites that handles all required categories in two seperate databases.

--- tyre_webscraper.py
+++ tyre_webscraper.py
@@ -5,7 +5,16 @@ from bs4 import BeautifulSoup
 import numpy as np 
 import csv
 import time 
-import random
+import random 
+
+def split_first(text):
+    # strip leading/trailing whitespace, then split into at most 2 parts
+    parts = text.strip().split(' ', 1)
+    first = parts[0]
+    rest  = parts[1] if len(parts) > 1 else ''
+    return first, rest
+
+
 # to implement replace the second half of the text with an input method for dimensions and postcode
 site = 'https://www.national.co.uk'  
 
@@ -16,7 +25,7 @@ rimSize = '16'
 postcode = '?pc=HU74DL'
 
 Isheader = True
-def tyre_database(site, append, width, aspectRatio, rimSize, postcode, isHeader):
+def Tyre_database_national(site, append, width, aspectRatio, rimSize, postcode, isHeader):
     fullWebsite = site + append + width + '-' + aspectRatio + '-' + rimSize + postcode
     # website = 'https://www.national.co.uk/tyres-search/205-55-16?pc=HU74DL'  
     csv_file = 'national_tyres.csv'
@@ -25,7 +34,6 @@ def tyre_database(site, append, width, aspectRatio, rimSize, postcode, isHeader)
     r = requests.get(fullWebsite) 
     soup = BeautifulSoup(r.text, 'html.parser') 
     prices = soup.find_all('span', class_='red text-24')  
-    names = soup.find_all('div', class_='details')  
     patterns = soup.find_all('a', class_='pattern_link') 
     formattedPrice = []   
     formattedPatterns = []
@@ -55,11 +63,70 @@ def tyre_database(site, append, width, aspectRatio, rimSize, postcode, isHeader)
     c = conn.cursor()  
     df = pd.read_csv(csv_file, encoding='ISO-8859-1') 
     df.info()
-    df.to_sql('national_tyres', conn, if_exists='replace', index=False)
+    df.to_sql('national_tyres', conn, if_exists='replace', index=False) 
+
+def Tyre_database_blackcircles(site, append, width, aspectRatio, rimSize, postcode, isHeader):
+    fullWebsite = site + append + width + '-' + aspectRatio + '-' + rimSize 
+    # website = 'https://www.blackcircles.com/tyres/205-55-16'  
+    csv_file = 'blackcircles.csv'
+
+    print(fullWebsite)
+    r = requests.get(fullWebsite) 
+    soup = BeautifulSoup(r.text, 'html.parser') 
+    prices = soup.find_all('div', class_='tyrePrice')   
+    patterns = soup.find_all('p', class_='model-size')  
+    # names = soup.find_all('span', class_='tyreNameWrap')  
+    #Since the class is a span, strip is used to remove any extra spaces
+    names = [s.get_text(strip=True) for s in soup.find_all('span', class_='tyreNameWrap')] 
+
+    # the tyre pattern and name are joined together so they must be seperated
+
+    formattedPrice = []   
+    formattedPatterns = []  
+    formattedNames = []
+
+    print(names) 
 
 
-tyre_database(site, append, '205', '55', '16', postcode, True) 
+    for price in prices:  
+        formattedPrice.append(price.text)
+
+    for name in names:    
+        first, rest = split_first(name) 
+        formattedNames.append(first)
+        formattedPatterns.append(rest)  
+    
+
+
+    # Create a CSV file and write the header and data
+    with open(csv_file, 'a', newline='') as file:  
+        writer = csv.DictWriter(file, fieldnames=['Website', 'Name', 'Width', 'Aspect ratio', 'Rim size', 'Pattern', 'Price'])
+        if isHeader:
+            writer.writeheader()
+        for i in range(0, len(formattedPrice)):    
+            res = " ".join(str(formattedPrice[i]).split())
+            row = {'Website' : str(site), 'Name' : str(formattedNames[i]), 'Width' : str(width) , 'Aspect ratio' : str(aspectRatio) , 'Rim size' : str(rimSize) , 'Pattern' : str(formattedPatterns[i])  , 'Price' : str(res)}
+            writer.writerow(row) 
+
+    conn = sqlite3.connect('blacktyres.db') 
+    c = conn.cursor()  
+    df = pd.read_csv(csv_file, encoding='ISO-8859-1') 
+    df.info()
+    df.to_sql('blacktyres', conn, if_exists='replace', index=False)
+
+
+site = 'https://www.national.co.uk'   
+append = '/tyres-search/'
+Tyre_database_national(site, append, '205', '55', '16', postcode, True) 
 time.sleep(random.randint(10, 30)) 
-tyre_database(site, append, '225', '50', '16', postcode, False) 
+Tyre_database_national(site, append, '225', '50', '16', postcode, False) 
 time.sleep(random.randint(10, 30)) 
-tyre_database(site, append, '185', '16', '14', postcode, False) 
+Tyre_database_national(site, append, '185', '16', '14', postcode, False) 
+
+site = 'https://www.blackcircles.com'   
+append = '/tyres/'
+Tyre_database_blackcircles(site, append, '205', '55', '16', postcode, True) 
+time.sleep(random.randint(10, 30)) 
+Tyre_database_blackcircles(site, append, '225', '50', '16', postcode, False) 
+time.sleep(random.randint(10, 30)) 
+Tyre_database_blackcircles(site, append, '185', '16', '14', postcode, False) 
