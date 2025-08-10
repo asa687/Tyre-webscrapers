9813ace1cca1e0949feb311028f7c23541b53f0a
asa687 <85872834+asa687@users.noreply.github.com>
2025-08-10 16:43:09 +0100

    changed the output into a single CSV and database both named tyres

--- tyre_webscraper.py
+++ tyre_webscraper.py
@@ -28,7 +28,7 @@ Isheader = True
 def Tyre_database_national(site, append, width, aspectRatio, rimSize, postcode, isHeader):
     fullWebsite = site + append + width + '-' + aspectRatio + '-' + rimSize + postcode
     # website = 'https://www.national.co.uk/tyres-search/205-55-16?pc=HU74DL'  
-    csv_file = 'national_tyres.csv'
+    csv_file = 'Tyres.csv'
 
     print(fullWebsite)
     r = requests.get(fullWebsite) 
@@ -59,16 +59,16 @@ def Tyre_database_national(site, append, width, aspectRatio, rimSize, postcode, 
             row = {'Website' : str(site), 'Name' : str(brandName), 'Width' : str(width) , 'Aspect ratio' : str(aspectRatio) , 'Rim size' : str(rimSize) , 'Pattern' : str(formattedPatterns[i])  , 'Price' : str(res)}
             writer.writerow(row) 
 
-    conn = sqlite3.connect('national_tyres.db') 
+    conn = sqlite3.connect('Tyres.db') 
     c = conn.cursor()  
     df = pd.read_csv(csv_file, encoding='ISO-8859-1') 
     df.info()
-    df.to_sql('national_tyres', conn, if_exists='replace', index=False) 
+    df.to_sql('Tyres', conn, if_exists='replace', index=False) 
 
 def Tyre_database_blackcircles(site, append, width, aspectRatio, rimSize, postcode, isHeader):
     fullWebsite = site + append + width + '-' + aspectRatio + '-' + rimSize 
     # website = 'https://www.blackcircles.com/tyres/205-55-16'  
-    csv_file = 'blackcircles.csv'
+    csv_file = 'Tyres.csv'
 
     print(fullWebsite)
     r = requests.get(fullWebsite) 
@@ -79,25 +79,20 @@ def Tyre_database_blackcircles(site, append, width, aspectRatio, rimSize, postco
     #Since the class is a span, strip is used to remove any extra spaces
     names = [s.get_text(strip=True) for s in soup.find_all('span', class_='tyreNameWrap')] 
 
-    # the tyre pattern and name are joined together so they must be seperated
-
     formattedPrice = []   
     formattedPatterns = []  
     formattedNames = []
 
-    print(names) 
-
-
+    
     for price in prices:  
         formattedPrice.append(price.text)
 
+    # Split the names into tyre name and pattern
     for name in names:    
         first, rest = split_first(name) 
         formattedNames.append(first)
         formattedPatterns.append(rest)  
     
-
-
     # Create a CSV file and write the header and data
     with open(csv_file, 'a', newline='') as file:  
         writer = csv.DictWriter(file, fieldnames=['Website', 'Name', 'Width', 'Aspect ratio', 'Rim size', 'Pattern', 'Price'])
@@ -108,11 +103,11 @@ def Tyre_database_blackcircles(site, append, width, aspectRatio, rimSize, postco
             row = {'Website' : str(site), 'Name' : str(formattedNames[i]), 'Width' : str(width) , 'Aspect ratio' : str(aspectRatio) , 'Rim size' : str(rimSize) , 'Pattern' : str(formattedPatterns[i])  , 'Price' : str(res)}
             writer.writerow(row) 
 
-    conn = sqlite3.connect('blacktyres.db') 
+    conn = sqlite3.connect('Tyres.db') 
     c = conn.cursor()  
     df = pd.read_csv(csv_file, encoding='ISO-8859-1') 
     df.info()
-    df.to_sql('blacktyres', conn, if_exists='replace', index=False)
+    df.to_sql('Tyres', conn, if_exists='replace', index=False)
 
 
 site = 'https://www.national.co.uk'   
@@ -125,7 +120,7 @@ Tyre_database_national(site, append, '185', '16', '14', postcode, False)
 
 site = 'https://www.blackcircles.com'   
 append = '/tyres/'
-Tyre_database_blackcircles(site, append, '205', '55', '16', postcode, True) 
+Tyre_database_blackcircles(site, append, '205', '55', '16', postcode, False) 
 time.sleep(random.randint(10, 30)) 
 Tyre_database_blackcircles(site, append, '225', '50', '16', postcode, False) 
 time.sleep(random.randint(10, 30)) 
